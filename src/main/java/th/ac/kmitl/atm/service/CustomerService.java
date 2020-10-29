84e37fe736261a35d46e7cb444d3c21962ba1232
ppanda-n <71163292+ppanda-n@users.noreply.github.com>
2020-10-29 21:38:13 +0700

    Add files via upload

--- src/main/java/th/ac/kmitl/atm/service/CustomerService.java
+++ src/main/java/th/ac/kmitl/atm/service/CustomerService.java
@@ -1,5 +1,5 @@
 package th.ac.kmitl.atm.service;
-
+import org.mindrot.jbcrypt.BCrypt;
 import org.springframework.stereotype.Service;
 import th.ac.kmitl.atm.model.Customer;
 
@@ -19,10 +19,40 @@ public class CustomerService {
     }
 
     public void createCustomer(Customer customer) {
+        String hashPin = hash(customer.getPin());
+        customer.setPin(hashPin);
         customerList.add(customer);
     }
 
     public List<Customer> getCustomers() {
         return new ArrayList<>(this.customerList);
+    }
+
+    public Customer findCustomer(int id) {
+        for (Customer customer : customerList) {
+            if (customer.getId() == id)
+                return customer;
+        }
+        return null;
+    }
+    public Customer checkPin(Customer inputCustomer) {
+        // 1. หา customer ที่มี id ตรงกับพารามิเตอร์
+        Customer storedCustomer = findCustomer(inputCustomer.getId());
+
+        // 2. ถ้ามี id ตรง ให้เช็ค pin ว่าตรงกันไหม โดยใช้ฟังก์ชันเกี่ยวกับ hash
+        if (storedCustomer != null) {
+            String hashPin = storedCustomer.getPin();
+
+            if (BCrypt.checkpw(inputCustomer.getPin(), hashPin))
+                return storedCustomer;
+        }
+        // 3. ถ้าไม่ตรง ต้องคืนค่า null
+        return null;
+    }
+
+
+    private String hash(String pin) {
+        String salt = BCrypt.gensalt(12);
+        return BCrypt.hashpw(pin, salt);
     }
 }
